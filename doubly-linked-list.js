379b53c9af2ea2296a2b16d6fbc4a877d62c6974
Cloudbop <boppincolin@gmail.com>
2020-04-02 12:38:36 +0100

    implemented shift and pop

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -13,7 +13,7 @@ class DoubleLinkedList {
     this.tail = null;
     this.length = 0;
   }
-  // push item to end
+  // push item to end and return list
   push(val) {
     let newNode = new Node(val);
     //
@@ -31,5 +31,55 @@ class DoubleLinkedList {
     this.length++;
     return this;
   }
+  // remove last item and return it
+  pop() {
+    // list is length 0
+    if (!this.tail) return undefined;
+    //
+    let removedNode = this.tail;
+    if (this.length === 1) {
+      // this pop is emptying our list
+      this.head = null;
+      this.tail = null;
+    } else {
+      // assign the prev to be tail
+      this.tail = removedNode.prev;
+      // the tail has no next
+      this.tail.next = null;
+      // sever connection from removedNode otherwise removedNode still access via it's prev
+      removedNode.prev = null;
+    }
+    //
+    this.length--;
+    return removedNode;
+  }
+  // remove first item from list
+  shift() {
+    //
+    if (!this.head) undefined;
+    let removedNode = this.head;
+    if (this.length === 1) {
+      // this pop is emptying our list
+      this.head = null;
+      this.tail = null;
+    } else {
+      // assign removed head.next to new head
+      this.head = removedNode.next;
+      // remove prev from new head
+      this.head.prev = null;
+      // sever connections on removedNode
+      removedNode.next = null;
+    }
+    this.length--;
+    return removedNode;
+  }
 }
-//
+
+let myTestList = new DoubleLinkedList();
+
+myTestList.push(100);
+myTestList.push(200);
+myTestList.push(400);
+myTestList.push(600);
+myTestList.push(800);
+myTestList.push(1000);
